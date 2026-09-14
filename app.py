3104b11d2d7fcbe0d406462f16f1a4b2d676e705
Luciuszhao12 <Luciusz2@nycstudents.net>
2026-09-14 10:21:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+
+def message(input):
+    print(input)
+message("Hello Class")
+
+def rectangle(x):
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+rectangle(200)
+
+def equal(x):
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+equal(200) 
+
+turtle.done()
