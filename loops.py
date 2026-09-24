169a11a258e68e5a2f0691e897372c234d43ede8
Luciuszhao12 <Luciusz2@nycstudents.net>
2026-09-24 10:12:24 -0400

    hkljhk

--- loops.py
+++ loops.py
@@ -2,9 +2,47 @@ import turtle
 from turtle import *
 t = Turtle()
 t.shape('turtle')
+t.speed(0)
 
-def square(length):
-    for i in range(4):
-        t.forward(length)
-        t.left(90)
 
+# def square(length):
+#     for i in range(4):
+#         t.forward(length)
+#         t.left(90)
+
+# def square(length,angle):
+#     for _ in range (4):
+#         t.forward(length)
+#         t.right(angle)
+
+# def drawSprial ():
+#     for i in range(60):
+#         square(100,90)
+#         t.right(5)
+# drawSprial()
+
+# def square(x,y):
+#     for i in range(4):
+#         t.forward(x)
+#         t.left(y)
+
+# def addSquares(iRange):
+#     length = 25
+#     for i in range(iRange):
+#         square(length, 90)
+#         length += 25
+# addSquares(5)
+
+
+# def square(length,angle):
+#     for i in range(4):
+#         t.forward(length)
+#         t.right(angle)
+
+# def addSquares(iRange):
+#     length = 5
+#     for i in range(iRange):
+#         square(length, 90)
+#         t.right(5)
+#         length += 5
+# addSquares(60)
